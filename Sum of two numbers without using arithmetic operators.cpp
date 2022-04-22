63dc86ac65b9134c78518cb6d254a3584fc33c80
hey rum ba <siddhartcool2000@gmail.com>
2022-04-22 21:55:35 +0530

    intial

---
+++ Sum of two numbers without using arithmetic operators.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    int sum(int a , int b)
+    {
+        //code here
+        int sum =0;
+    if (not b)
+        {return a;}
+   int carry = a&b;
+    a = a^b;
+    b = carry<<1;
+   return a+b;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b;
+        cin >> a>>b;
+        
+        Solution ob;
+        cout<< ob.sum(a,b) <<"\n";
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
