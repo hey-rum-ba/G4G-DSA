daaa0723ae871bdaaaa588c6c31197f82fba27fc
hey rum ba <siddhartcool2000@gmail.com>
2022-04-23 21:29:47 +0530

    initial

---
+++ Search insert position of K in a sorted array.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int searchInsertK(vector<int>Arr, int N, int k)
+    {
+        // code here
+         if(k<=Arr[0])
+       {return 0;}
+       for(int i=0;i<N;i++)
+       {
+           if(k>=Arr[i] && k<=Arr[i+1])
+           return i+1;
+       }
+
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        vector<int>Arr(N);
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.searchInsertK(Arr, N, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
