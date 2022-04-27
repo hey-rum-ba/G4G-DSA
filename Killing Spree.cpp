78a00e676ac415024b77f7deb5b6dc91a1f7e208
hey rum ba <siddhartcool2000@gmail.com>
2022-04-27 19:33:36 +0530

    initial

---
+++ Killing Spree.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+public:
+    long long int killinSpree(long long int n)
+    {
+        // Code Here
+       long X=1;
+       long ans=0;
+       int i=1;
+       while(n>=X){
+           X=i*i;
+           if(n>=X){
+               ans++;
+           }
+           n=n-X;
+           i++;
+       }
+       return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n; cin>>n;
+        Solution obj;
+        long long int ans = obj.killinSpree(n);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
