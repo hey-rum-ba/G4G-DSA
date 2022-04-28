c1f43e18e97a935d80d693706d258f7a4a956acc
hey rum ba <siddhartcool2000@gmail.com>
2022-04-28 20:09:40 +0530

    initial

---
+++ Shop in Candy Store.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> candyStore(int candies[], int N, int K)
+    {
+        // Write Your Code here
+        sort(candies,candies+N);
+        vector<int> res(2,0);
+        for(int i=0;i<N-K*i;i++){
+            res[0]+=candies[i];
+        }
+        for(int i=N-1;i>=K*(N-i-1);i--){
+            res[1]+=candies[i];
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        int candies[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> candies[i];
+        }
+
+        Solution ob;
+        vector<int> cost = ob.candyStore(candies, N, K);
+        cout << cost[0] << " " << cost[1] << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
