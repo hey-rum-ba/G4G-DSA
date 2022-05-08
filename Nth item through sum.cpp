09d97afa2103a2764ba5aa421f2dc1a41c0db8cf
hey rum ba <siddhartcool2000@gmail.com>
2022-05-08 21:54:11 +0530

    initial

---
+++ Nth item through sum.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int nthItem(int L1, int L2, int A[], int B[], int N)
+    {
+        // code here
+       vector<int> arr;
+       set<int> s;
+       for(int i=0; i<L1; i++){
+           for(int j=0; j<L2; j++){
+              int temp = A[i]+B[j];
+              arr.push_back(temp);
+           }
+       }
+       for(int i=0; i<arr.size(); i++){
+           s.insert(arr[i]);
+       }
+      copy(s.begin(), s.end(), arr.begin());
+       int siz= s.size();
+       if(siz<N-1)
+       return -1;
+       else
+       return arr[N-1];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int L1, L2, N;
+        cin>>L1>>L2;
+        int A[L1], B[L2];
+        for(int i = 0;i < L1;i++)
+            cin>>A[i];
+        for(int i = 0;i < L2;i++)
+            cin>>B[i];
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.nthItem(L1, L2, A, B, N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
