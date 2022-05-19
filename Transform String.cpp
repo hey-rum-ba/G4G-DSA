393127103e832464ed91c1cf4b6d4f1485e7c9d9
hey rum ba <siddhartcool2000@gmail.com>
2022-05-19 13:07:38 +0530

    initial

---
+++ Transform String.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    int transform (string A, string B)
+    {
+        //code here.
+        int counter = 0;
+    int i = B.size() - 1;
+    if(A.size() != B.size()) return -1;
+ 
+    for(int j = A.size()-1; j >= 0; j--){
+         if(A[j] != B[i])
+             counter++;
+         else
+             i--;
+    }
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    if(A != B) return -1;
+    return counter;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main () 
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string A, B; 
+        cin >> A >> B;
+        Solution ob;
+        cout <<ob.transform (A, B) << endl;
+    }
+}  // } Driver Code Ends
