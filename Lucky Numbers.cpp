b8cf422dc58a2b64a87c616deb80ac3ed51fae08
hey-rum-ba <siddhartcool2000@gmail.com>
2022-05-28 20:10:49 +0530

    initial

---
+++ Lucky Numbers.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+    bool checkbyindex(int index,int start)
+   {
+       int count=0;
+       if(index<start) return true;
+       int i=1;
+       
+       while(start*i<index)
+       {
+           count++;
+           i++;
+       }
+       if(start*i==index)
+       {
+           return false;
+       }
+       
+       index=index-count;
+       checkbyindex(index,start+1);
+   }
+public:
+bool isLucky(int n) {
+    // code here
+    
+    int index=n;
+    bool flag =checkbyindex(n,2);
+    return flag;
+    
+}
+};
+
+
+// { Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}  // } Driver Code Ends
