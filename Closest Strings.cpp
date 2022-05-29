6afe370a5eed7b1080ffb20fa7e906679d7c4e7a
hey-rum-ba <siddhartcool2000@gmail.com>
2022-05-29 19:51:34 +0530

    initial

---
+++ Closest Strings.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int shortestDistance(vector<string> &s, string word1, string word2)
+	{
+		// Your code goes here
+		int cnt1=-1,cnt2=-1,distance=INT_MAX;
+		for(int i=0;i<s.size();i++)
+		{
+		   if(s[i]==word1) cnt1=i;
+		   if(s[i]==word2) cnt2=i;
+		   if(cnt1!=-1 and cnt2!=-1) distance=min(distance,abs(cnt1-cnt2));
+		 }
+         return distance;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+
+   		vector<string> s(n);
+   		for(int i = 0; i < n; i++)
+   			cin >> s[i];
+
+   		string word1, word2;
+
+   		cin >> word1 >> word2;
+   		
+   		Solution ob;
+
+   		cout << ob.shortestDistance(s, word1, word2) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
