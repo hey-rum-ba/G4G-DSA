5c182ef9be83b0a7c1e62ec1064f6289a78a58d6
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-01 13:14:17 +0530

    initial

---
+++ Nth node from end of linked list.cpp
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       struct Node* fast = head, *slow = head;
+       int cnt = 0;
+       while(cnt < n and fast){
+           cnt++;
+           fast = fast->next;
+       }
+       if(cnt < n){
+           return -1;
+       }
+       while(slow and fast){
+           slow = slow->next;
+           fast = fast->next;
+       }
+       if(fast == nullptr){
+           return slow->data;
+       }
+       return -1;
+}
+
