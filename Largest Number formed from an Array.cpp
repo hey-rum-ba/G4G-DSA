cc697019c6e169e402948930877ec5f18eb7302c
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-05 18:40:51 +0530

    initial

---
+++ Largest Number formed from an Array.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+    static bool stringComprator(string a, string b){
+	    return a+b>b+a;
+	}
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	string printLargest(vector<string> &arr) {
+	    // code here
+	    string ans="";
+	    sort(arr.begin(),arr.end(),stringComprator);
+	    for(string s : arr)
+	        ans+=s;
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
