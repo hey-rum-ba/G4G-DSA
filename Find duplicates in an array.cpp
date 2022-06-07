2a7f73fe5f52f2f53f6ebfd801fd8555d01e7c7b
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-07 21:01:27 +0530

    initial

---
+++ Find duplicates in an array.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+        vector<int>v; int flag = 0;
+        sort(arr,arr+n);
+        for(int i=0; i<n; i++) {
+           if(arr[i] == arr[i+1]) {
+               flag++;
+               if(flag == 1) {
+                   v.push_back(arr[i]);
+               }
+           }
+           else {
+               flag = 0;
+           }
+        }
+        // return (v.size()>0)? v : -1;
+        if(v.size() > 0) return v;
+        return {-1};
+       
+       
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
