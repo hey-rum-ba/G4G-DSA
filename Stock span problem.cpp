6f7b7a89aac2a2a35f6fada4c0c37f7ff792edc5
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-07 15:44:38 +0530

    initial

---
+++ Stock span problem.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       vector<int>ans(n,1);
+       for(int i=0;i<n;i++){
+           int index = i-1;
+           int cnt = 0;
+           while(index>=0 && price[index]<= price[i]){
+               cnt+= ans[index];
+               index-= ans[index];
+           }
+           ans[i]+=cnt;
+       }
+       return ans;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
