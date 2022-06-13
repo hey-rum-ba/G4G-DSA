a9db157eaedd69930adf9a1444500d8be5e8abad
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-13 19:08:15 +0530

    initial

---
+++ String formation from substring.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isRepeat(string s)
+	{
+	    // Your code goes here
+	    return ((s+s).find(s,1)==s.size()) ? 0 : 1;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+        Solution ob;
+   		cout << ob.isRepeat(str) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
