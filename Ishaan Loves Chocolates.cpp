d3a2d693b8f5ec266342c384413d592a122e3245
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-15 15:42:41 +0530

    initial

---
+++ Ishaan Loves Chocolates.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int chocolates(int arr[], int n);
+
+
+int main()
+{
+    
+    int t;cin>> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        
+        cout << chocolates(arr, n);
+        cout << endl;
+        
+    }
+
+}
+// } Driver Code Ends
+
+
+int chocolates(int arr[], int n)
+{
+    // Complete the function
+    int l=0,r=n-1;
+    while(l<r){
+        if(arr[l]>arr[r])
+            l++;
+        else
+            r--;
+    }
+    return arr[l];
+    
+}
