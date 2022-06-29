8d0ec64d6dc46facfec576d3cbd89de31c558b77
hey-rum-ba <siddhartcool2000@gmail.com>
2022-06-29 12:31:36 +0530

    initial

---
+++ Jump Game.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        int step=N-1;
+        for(int i=N-2;i>=0;i--)
+        {
+            if((A[i]+i)>=step)
+            step=i;
+        }
+        if(step==0) return 1;
+        return 0;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
