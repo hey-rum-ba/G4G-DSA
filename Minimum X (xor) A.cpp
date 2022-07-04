7527243f53ab0368be48ca0a4cb35a6dfdb520fa
hey-rum-ba <siddhartcool2000@gmail.com>
2022-07-04 17:30:27 +0530

    initial

---
+++ Minimum X (xor) A.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minVal(int a, int b) {
+        // code here
+        bitset<32>b1(a);
+        bitset<32>b2(b);
+        int a_count = b1.count();
+        int b_count = b2.count();
+        int diff = abs(a_count - b_count);
+        if(diff == 0){
+            return a;
+        }
+        else if(a_count > b_count){
+            while(diff > 0){
+                a = a & (a - 1);
+                diff--;
+            }
+        }
+        else{
+            while(diff > 0){
+                a = a | (a + 1);
+                diff--;
+            }
+        }
+        return a;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        Solution ob;
+        cout << ob.minVal(a, b);
+
+        cout << "\n";
+    }
+
+    return 0;
+}  // } Driver Code Ends
