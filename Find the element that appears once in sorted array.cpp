56a941a3e8319ab7aa796a5a59262512051cb01c
hey-rum-ba <siddhartcool2000@gmail.com>
2022-07-28 16:14:21 +0530

    initial

---
+++ Find the element that appears once in sorted array.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Driver code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int findOnce(int arr[], int n)
+    {
+        //code here.
+        int start=0;
+        int end=n-2;
+        while(start<=end){
+            int mid=(end+start)>>1;
+            if(arr[mid]==arr[mid^1])
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return arr[start];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+        {
+            cin>>A[i];
+        }
+        
+        Solution ob;
+        
+        int res = ob.findOnce(A,n);
+        cout << res << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
