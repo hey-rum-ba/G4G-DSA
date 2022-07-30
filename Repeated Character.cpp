1829e76e527f09e1ea88d58d487062f516ef2493
hey-rum-ba <siddhartcool2000@gmail.com>
2022-07-30 19:20:06 +0530

    initial

---
+++ Repeated Character.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        for(int i = 0; i < s.size() - 1; i++){
+            if(s.find(s[i], i+1) != std::string::npos) return s[i];
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
