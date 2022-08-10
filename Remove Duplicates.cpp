ee4919f4bf1d384a4053ee6079deb622ab62dad0
hey-rum-ba <siddhartcool2000@gmail.com>
2022-08-10 20:49:24 +0530

    initial

---
+++ Remove Duplicates.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeDups(string S) 
+	{
+	    // Your code goes here
+	    string ans= "";
+	    bool check[26] = {0};
+	    for(int i=0;i<S.size();i++){
+	        if(check[S[i]-'a']==0) {ans+=S[i];check[S[i]-'a'] =1;}
+	        else continue;
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		
+   		Solution ob;
+   		cout << ob.removeDups(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
