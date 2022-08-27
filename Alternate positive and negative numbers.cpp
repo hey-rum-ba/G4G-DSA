9a8ace09f95a8e95debae330d8a57ae775232826
hey-rum-ba <siddhartcool2000@gmail.com>
2022-08-27 22:30:03 +0530

    initial

--- Alternate positive and negative numbers.cpp
+++ Alternate positive and negative numbers.cpp
@@ -10,7 +10,19 @@ public:
 
 	void rearrange(int arr[], int n) {
 	    // code here
-	    stack<int>positive,negative;
+	    //{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    // code here
+	    queue<int>positive,negative;
 	    for(int i=0;i<n;i++){
 	        if (0 <= arr[i])
                 positive.push(arr[i]);
@@ -19,12 +31,38 @@ public:
 	    }
 	    for(int i=0;i<n;i++){
 	        if(!positive.empty() and !negative.empty()){
-	            if(i%2==0){ arr[i]=positive.top();positive.pop();}
-	            else {arr[i]=negative.top();negative.pop();}
+	            if(i%2==0){ arr[i]=positive.front();positive.pop();}
+	            else {arr[i]=negative.front();negative.pop();}
 	        }
-	        else if(positive.empty()) {arr[i]= negative.top();negative.pop();}
-	        else {arr[i]= positive.top();positive.pop();}
+	        else if(positive.empty()) {arr[i]= negative.front();negative.pop();}
+	        else {arr[i]= positive.front();positive.pop();}
 	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
 	}
 };
 
