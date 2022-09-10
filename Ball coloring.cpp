697c14b766418f80b89f63e47800c37f3e9acf20
hey-rum-ba <siddhartcool2000@gmail.com>
2022-09-10 17:33:49 +0530

    initial

---
+++ Ball coloring.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    unsigned long long int noOfWays(unsigned long long int n){
+        // code here
+        if(n<=3){
+           return pow(2,n);
+        }
+        else{
+            return (n*(n-1)+2);
+            
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        unsigned long long int n;
+        cin >> n;
+        Solution ob;
+        cout<<ob.noOfWays(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
