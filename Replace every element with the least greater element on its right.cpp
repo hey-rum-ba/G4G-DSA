22dfa407c85d893ba922ee4e384c035ac1912cd1
hey-rum-ba <siddhartcool2000@gmail.com>
2022-10-17 17:11:24 +0530

    initial

---
+++ Replace every element with the least greater element on its right.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+    vector<int> findLeastGreater(vector<int>& arr, int n) {
+        vector<int> ans(n,-1);
+        
+        set<int> st;
+        
+        for(int i=n-1;i>=0;i--){
+            auto itr = st.upper_bound(arr[i]);
+            
+            if(itr != st.end()){
+                ans[i] = *itr;
+            }
+            
+            st.insert(arr[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    vector<int>arr(n);
+	    for(int i = 0 ; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findLeastGreater(arr, n);
+	    for(auto it : ans){
+	        cout << it << " ";
+	    }
+	    cout <<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
