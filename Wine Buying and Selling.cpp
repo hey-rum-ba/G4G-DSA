6cdb95e7830e170b5bccc648d2acfc28c5828988
hey-rum-ba <siddhartcool2000@gmail.com>
2022-12-24 11:58:59 +0530

    initial

---
+++ Wine Buying and Selling.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+  public:
+  long long int wineSelling(vector<int>& Arr, int N){
+      //Code here.
+      long long int sum = 0;
+      long long int a = 0;
+      for(int i=0;i<N;i++){
+          a += Arr[i];
+          sum += abs(a);
+      }
+      return sum;
+  }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        long long int ans = ob.wineSelling(arr,n);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
