eac86afb6099b6fa2eab552b1ffb1f1566ab29a3
hey-rum-ba <siddhartcool2000@gmail.com>
2023-01-16 23:03:43 +0530

    initial

--- Next Greater Element.cpp
+++ Next Greater Element.cpp
@@ -23,6 +23,31 @@ class Solution
 
         return nge;
     }
+
+//     vector<long long> nextLargerElement(vector<long long> &arr, int n)
+//     {
+//         // Your code here
+//         vector<long long> ans(n, 0);
+//         stack<long long> stk;
+//         for (int i = n - 1; i >= 0; i--)
+//         {
+//             while (stk.size() > 0 && stk.top() <= arr[i])
+//             {
+//                     stk.pop();
+//             }
+//             if (stk.size() == 0)
+//             {
+//                     ans[i] = -1;
+//             }
+//             else
+//             {
+//                     ans[i] = stk.top();
+//             }
+//             stk.push(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
 };
 
 //{ Driver Code Starts.
