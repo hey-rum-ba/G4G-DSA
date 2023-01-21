f9f8421b0c13bf3e16b74fc1841410df50e84b09
hey-rum-ba <siddhartcool2000@gmail.com>
2023-01-21 17:23:24 +0530

    initial

--- Minimum X (xor) A.cpp
+++ Minimum X (xor) A.cpp
@@ -35,6 +35,104 @@ class Solution {
     }
 };
 
+// class Solution
+// {
+// public:
+//     int minVal(int a, int b)
+//     {
+
+//         // code here
+
+//         int sum = a;
+
+//         int cnta = 0;
+
+//         int cntb = 0;
+
+//         for (int i = 0; i < 32; i++)
+
+//         {
+
+//             if ((1 << i) & a)
+
+//             {
+
+//                 cnta++;
+//             }
+
+//             if ((1 << i) & b)
+
+//             {
+
+//                 cntb++;
+//             }
+//         }
+
+//         if (cnta == cntb)
+
+//         {
+
+//             return a;
+//         }
+
+//         else if (cnta > cntb)
+
+//         {
+
+//             int sum = a;
+
+//             for (int i = 0; i < 32; i++)
+
+//             {
+
+//                 if ((1 << i) & a)
+
+//                 {
+
+//                     sum = sum ^ (1 << i);
+
+//                     cnta--;
+
+//                     if (cnta == cntb)
+
+//                     {
+
+//                         return sum;
+//                     }
+//                 }
+//             }
+//         }
+
+//         else
+
+//         {
+
+//             int sum = a;
+
+//             for (int i = 0; i < 32; i++)
+
+//             {
+
+//                 if (((1 << i) & a) == 0)
+
+//                 {
+
+//                     sum = sum | (1 << i);
+
+//                     cnta++;
+
+//                     if (cnta == cntb)
+
+//                     {
+
+//                         return sum;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// };
+
 // { Driver Code Starts.
 
 int main() {
