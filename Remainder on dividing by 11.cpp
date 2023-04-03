9436199b94eb2519e4b157785d74236fd81af7b7
hey-rum-ba <siddhartcool2000@gmail.com>
2023-04-03 21:49:21 +0530

    initial

---
+++ Remainder on dividing by 11.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+
+public:
+    int xmod11(string x)
+    {
+        // code here
+        int ans = 0;
+        int n = x.length();
+        for (int i = 0; i < n; i++)
+            ans = (ans * 10 + (int)(x[i] - '0')) % 11;
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin >> x;
+        Solution ob;
+        int ans = ob.xmod11(x);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
