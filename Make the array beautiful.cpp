49ed447f4c6650a673d7139685ffb49613dc7f6c
hey-rum-ba <siddhartcool2000@gmail.com>
2023-04-08 23:24:45 +0530

    initial

---
+++ Make the array beautiful.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    vector<int> makeBeautiful(vector<int> arr)
+    {
+        // code here
+        vector<int> res;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (res.size() == 0)
+                res.push_back(arr[i]);
+            else
+            {
+                if ((res.back() > 0 && arr[i] < 0) or (res.back() < 0 && arr[i] > 0) or
+                    (arr[i] == 0 and res.back() < 0) or (res.back() == 0 and arr[i] < 0))
+                    res.pop_back();
+                else
+                    res.push_back(arr[i]);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++)
+        {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
