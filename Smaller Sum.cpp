c36ee057ab71087bf0d3ebb84e8c9c85660f2e96
hey-rum-ba <siddhartcool2000@gmail.com>
2023-04-22 20:08:45 +0530

    initial

---
+++ Smaller Sum.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    vector<long long> smallerSum(int n, vector<int> &arr)
+    {
+        // Code here
+        vector<vector<ll>> v;
+        for (int i = 0; i < n; i++)
+            v.push_back({i, (ll)arr[i]});
+
+        sort(v.begin(), v.end(), [](const vector<ll> &a, const vector<ll> &b)
+             { return a[1] < b[1]; });
+
+        for (int i = 1; i < n; i++)
+            v[i][1] += v[i - 1][1];
+        vector<ll> res(n, 0);
+
+        res[v[0][0]] = 0;
+        int idx = -1;
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[v[i][0]] == arr[v[i - 1][0]])
+                res[v[i][0]] = (idx == -1 ? 0 : v[idx][1]);
+            else
+            {
+                idx = i - 1;
+                res[v[i][0]] = v[idx][1];
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<long long> ans = ob.smallerSum(n, arr);
+        for (int i = 0; i < n; i++)
+        {
+            if (i != n - 1)
+            {
+                cout << ans[i] << " ";
+            }
+            else
+            {
+                cout << ans[i] << endl;
+            }
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
