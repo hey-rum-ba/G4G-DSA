b77a2c7162c8edd1e65f5a94a03d2cd1a45d1a91
hey-rum-ba <siddhartcool2000@gmail.com>
2023-05-29 23:12:12 +0530

    initial

---
+++ CamelCase Pattern Matching.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    bool matchPattern(string &word, string &patternToMatch)
+    {
+        string patternInWord = "";
+
+        for (char c : word)
+        {
+            if (isupper(c))
+                patternInWord += c;
+            if (patternInWord == patternToMatch)
+                return true;
+        }
+
+        return patternInWord == patternToMatch;
+    }
+    vector<string> CamelCase(int N, vector<string> Dictionary, string Pattern)
+    {
+        // code here
+        vector<string> ans;
+        for (string word : Dictionary)
+        {
+            if (matchPattern(word, Pattern))
+            {
+                ans.push_back(word);
+            }
+        }
+
+        sort(begin(ans), end(ans));
+
+        if (ans.empty())
+            ans.push_back("-1");
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        vector<string> Dictionary(N);
+        for (int i = 0; i < N; i++)
+            cin >> Dictionary[i];
+        string Pattern;
+        cin >> Pattern;
+        Solution ob;
+        vector<string> ans = ob.CamelCase(N, Dictionary, Pattern);
+        sort(ans.begin(), ans.end());
+        for (auto u : ans)
+            cout << u << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
