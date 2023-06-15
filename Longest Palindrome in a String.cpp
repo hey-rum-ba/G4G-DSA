7601311861e6250b2ab1365e1b40b4e036cf2295
hey-rum-ba <siddhartcool2000@gmail.com>
2023-06-15 22:40:59 +0530

    initial

--- Longest Palindrome in a String.cpp
+++ Longest Palindrome in a String.cpp
@@ -57,6 +57,37 @@ class Solution {
        for(int i=strt;i<=end;i++)
         ans+=s[i];
        return ans;
+       /*string longestPalin (string s) {
+        // code here
+        int N = s.size();
+        int l = -1, r = -1;
+        int len = 0;
+        for(int i = 0; i < N; i++) {
+            // odd length
+            int d = 0;     // d -> distance from mid
+            while( i-d >= 0 && i+d < N && s[i-d] == s[i+d]) {
+                int nlen = 2*d + 1;
+                if(nlen > len) {
+                    len = nlen;
+                    l = i - d;
+                    r = i + d;
+                }
+                d++;
+            }
+            // even length
+            d = 0;
+            while( i-d+1 >= 0 && i+d < N && s[i-d+1] == s[i+d]) {
+                int nlen = 2*d;
+                if(nlen > len) {
+                    len = nlen;
+                    l = i - d+1;
+                    r = i + d;
+                }
+                d++;
+            }
+        }
+        return s.substr(l, len);
+        */
     }
 };
 
