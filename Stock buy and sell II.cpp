100b5500bb572bbd100bea35cf9d681ee9f1d3a4
hey-rum-ba <siddhartcool2000@gmail.com>
2023-07-05 19:41:30 +0530

    initial

---
+++ Stock buy and sell II.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int stockBuyAndSell(int n, vector<int> &prices)
+    {
+        // code here
+        int dp[n + 1][n + 1];
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int buy = 0; buy < 2; buy++)
+            {
+                long long profit = 0;
+
+                if (buy)
+                {
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                }
+                else
+                {
+                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+
+                dp[i][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+
+        int n;
+        scanf("%d", &n);
+
+        vector<int> prices(n);
+        Array::input(prices, n);
+
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
