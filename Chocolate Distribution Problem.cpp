91cbb5674fc08dadeb99c18187116ad871493193
hey-rum-ba <siddhartcool2000@gmail.com>
2023-08-05 22:45:31 +0530

    initial

--- Chocolate Distribution Problem.cpp
+++ Chocolate Distribution Problem.cpp
@@ -23,6 +23,22 @@ class Solution{
     return diff;
     }    
 };
+/*
+class Solution{
+	public:
+	long long findMinDiff(vector<long long> a, long long n, long long m){
+	//code
+	sort(a.begin(),a.end());
+	long long d=INT_MAX;
+	int i=0,j=m-1;
+	for(int i=0;j<n;i++,j++){
+		d = min(d,a[j]-a[i]);
+	}
+	return d;
+	}
+};
+
+*/
 
 //{ Driver Code Starts.
 int main() {
