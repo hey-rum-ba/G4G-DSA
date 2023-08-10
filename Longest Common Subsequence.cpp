dad70edd4c9d9bbb1957a15c36ffe1fd7e90458c
hey-rum-ba <siddhartcool2000@gmail.com>
2023-08-10 20:13:18 +0530

    initial

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+const int mod = 1e9 + 7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+public:
+    // Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        // your code here
+        int kp[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    kp[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    kp[i][j] = 1 + kp[i - 1][j - 1];
+                }
+                else
+                {
+                    kp[i][j] = max(kp[i - 1][j], kp[i][j - 1]);
+                }
+            }
+        }
+        return kp[n][m];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t, n, m;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m; // Take size of both the strings as input
+        string s1, s2;
+        cin >> s1 >> s2; // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(n, m, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
