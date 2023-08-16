8be7d70479744054457afd7008fb3b5da91a8994
hey-rum-ba <siddhartcool2000@gmail.com>
2023-08-16 21:54:02 +0530

    initial

---
+++ Nth catalan number.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to find the nth catalan number.
+    int mod = 1e9 + 7;
+    int topDown(int n)
+    {
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+
+            for (int j = 0; j < i; j++)
+            {
+                dp[i] = (dp[i] + (dp[j] * dp[i - 1 - j]) % mod) % mod;
+            }
+        }
+        return dp[n];
+    }
+    int findCatalan(int n)
+    {
+        // code here
+        vector<int> dp(n + 1, -1);
+        return topDown(n);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.findCatalan(n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
