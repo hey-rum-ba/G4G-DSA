6a6ee1fb1b1c0b970eafe64527da95d6b03eea96
hey-rum-ba <siddhartcool2000@gmail.com>
2023-08-27 18:30:54 +0530

    initial

---
+++ Reverse a String.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int l = 0;
+        int r = str.length() - 1;
+        while (l < r)
+        {
+            swap(str[l], str[r]);
+            l++;
+            r--;
+        }
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.reverseWord(s) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
