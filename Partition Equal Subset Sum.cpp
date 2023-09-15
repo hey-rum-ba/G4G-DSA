5399b2b0c3081c0e9a8196d651d01cf34df2f2dc
hey-rum-ba <siddhartcool2000@gmail.com>
2023-09-15 21:28:10 +0530

    initial

--- Partition Equal Subset Sum.cpp
+++ Partition Equal Subset Sum.cpp
@@ -43,7 +43,31 @@ public:
         return dp[n][target];
     }
 };
+/*
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        for(int i = 0; i < N; i++)
+            sum += arr[i];
+        if(sum % 2 != 0)
+            return 0;
 
+        int dp[sum/2 + 1]; memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+
+        for(int i = N - 1; i >= 0; i--){
+            for(int target = sum/2; target >= 0; target--){
+                if(target >= arr[i])
+                    dp[target] |= dp[target - arr[i]];
+            }
+        }
+        return dp[sum/2];
+    }
+};
+*/
 // { Driver Code Starts.
 
 int main(){
